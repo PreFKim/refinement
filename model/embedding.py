f9e63e79e41bf92720220373e170d1ce6b8715aa
PreFKim <pre_f_86@naver.com>
2024-05-13 12:05:23 +0900

    Rename rotary_embedding_torch.py -> embedding.py

--- model/embedding.py
+++ model/embedding.py
@@ -5,6 +5,8 @@ import torch
 from einops import rearrange, repeat
 from torch import einsum, nn
 
+import numpy as np
+
 # helper functions
 
 
@@ -130,3 +132,26 @@ class RotaryEmbedding(nn.Module):
             self.cache[cache_key] = freqs
 
         return freqs
+
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.1, max_len=500, batch_first=False):
+        super().__init__()
+        self.batch_first = batch_first
+
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * (-np.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        if self.batch_first:
+            x = x + self.pe.permute(1, 0, 2)[:, : x.shape[1], :]
+        else:
+            x = x + self.pe[: x.shape[0], :]
+        return self.dropout(x)
